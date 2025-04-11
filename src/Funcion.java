1b365a9d46f025f812e5f3bb804a9bc02e425484
fernando mongua <lfmonguaa@udistrital.edu.co>
2025-04-11 13:39:19 -0500

    quedo como propietario del archivo

--- src/Funcion.java
+++ src/Funcion.java
@@ -1,13 +1,40 @@
 public class Funcion {
     //Atributos para nuestro ejemplo
     int lado;
+    double altura;
     double radio;
     short base;
+    double pi=3.141592;
     //Metodos para nuestro ejemplo
 
-    public short calcularCubo(short base)
-    {
-        return (short) (base*base*base);
+    public short calcularCubo(short base) {
+
+        return (short) (base * base * base);
+    }
+
+    public int calcularAreaCuadrado(int lado) {
+
+        return  (lado * lado);
+    }
+    public double CalcularAreaTriangulo(short base,double altura){
+
+        return  ((base*(altura*altura))/2);
+    }
+    public double CalcularVolumenCubo(int lado) {
+
+        return (lado * lado * lado);
+    }
+    public double CalcularVolumenEsfera(double radio,double pi) {
+
+        return ((double) 4 /3*(pi)*(radio*radio*radio));
+    }
+    public double CalcularPerimetroCirculo(double radio,double pi) {
+
+        return (2*pi*radio);
+    }
+    public int CalcularPerimetroPentagono(int lado) {
+
+        return (lado+lado+lado+lado+lado);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,24 @@ public class Main {
         //Crear un objeto de tipo funciono llamado algoraro
         Funcion algoraro = new Funcion();
         algoraro.base=10;
+        algoraro.lado=2;
+        algoraro.altura=3;
+        algoraro.radio=7;
         short cubo = algoraro.calcularCubo(algoraro.base);
+        int cuadrado = algoraro.calcularAreaCuadrado(algoraro.lado);
+        double triangulo = algoraro.CalcularAreaTriangulo(algoraro.base, algoraro.altura);
+        double cuboc= algoraro.CalcularVolumenCubo(algoraro.lado);
+        double cuboe= algoraro.CalcularVolumenEsfera(algoraro.radio, algoraro.pi);
+        double perimetroc= algoraro.CalcularPerimetroCirculo(algoraro.radio, algoraro.pi);
+        int perimetrop= algoraro.CalcularPerimetroPentagono(algoraro.lado);
 
-        System.out.println(cubo);
-
+        System.out.println("el cubo del numero es "+cubo);
+        System.out.println("el cuadrado de " + cuadrado);
+        System.out.println("el area del triangulo es "+triangulo);
+        System.out.println("el volumen del cubo es "+cuboc);
+        System.out.println("el volumen de la esfera es "+cuboe);
+        System.out.println("el perimetro del circulo es"+ perimetroc);
+        System.out.println("el perimetro del pentagono es "+perimetrop);
 
     }
     public static String person(String name, int year){
